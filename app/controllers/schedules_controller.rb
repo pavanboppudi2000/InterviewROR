a3f76e6b612f094871b101cf464428d78d2e7ab4
inspiron <pavanboppudi20122000@gmail.com>
2020-09-17 18:09:43 +0530

    CRUD operations on Schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -57,6 +57,73 @@ class SchedulesController < ApplicationController
         end
         render 'new'
     end
+    def edit
+        @schedule=Schedule.find(params[:id])
+    end
+    def update
+        @schedule=Schedule.new(schedule_params)
+        @sch = Schedule.find(params[:id])
+        @inp1=Interviewer.where(email: @schedule.email1).count(:email)
+        @inp2=Interviewee.where(email: @schedule.email2).count(:email)
+        @eor=0
+        if @inp1<1 || @inp2<1
+            if @inp1<1
+              flash.alert="Email 1 is not a valid Interviewer"
+              @eor=@eor+1
+            end
+            if @inp2<1 
+              flash.alert="Email 2 is not a valid Interviewee"
+              @eor=@eor+1
+            end          
+        end        
+        @por=0
+        @inp3=Schedule.where(email1: @schedule.email1)
+        @inp3.each do |inp3|
+            if inp3.id == @sch.id
+                @por=0
+            else
+                if inp3.st < @schedule.st
+                    if inp3.end > @schedule.st
+                        flash.alert="Interviewer is not available"
+                        @eor=@eor+1
+                    end
+                elsif inp3.st < @schedule.end
+                        flash.alert="Interviewer is not available"
+                        @eor=@eor+1
+                end
+            end            
+        end
+        @inp4=Schedule.where(email2: @schedule.email1)
+        @inp4.each do |inp4|
+            if inp4.id == @sch.id
+                @por=0
+            else
+                if inp4.st < @schedule.st
+                    if inp4.end > @schedule.st
+                        flash.alert="Interviewee is not available"
+                        @eor=@eor+1
+                    end
+                elsif inp4.st< @schedule.end
+                     flash.alert="Interviewee is not available"
+                     @eor=@eor+1
+                end
+            end
+        end
+        if @eor>0
+            @eor=@eor+1
+        else
+            if(@sch.update(schedule_params))
+                redirect_to @sch
+                return 
+            end
+        end
+        render 'edit'
+    end
+    def destroy
+        @schedule=Schedule.find(params[:id])
+        @schedule.destroy
+        redirect_to schedules_path
+    end
     private
      def schedule_params
         params.require(:schedule).permit(:email1, :email2, :st , :end)
